635733fb1dcd9ab713600e4cc512c5b6721d6d23
Nazia Habib <habib.nazia@gmail.com>
2021-12-03 21:52:32 -0600

    Update employee_projects.sql

--- employee_projects.sql
+++ employee_projects.sql
@@ -18,13 +18,13 @@ create table employees (
 
 
 insert into employees
-	values (1, 'A', 2), (2, 'C', 4), (3, 'D', 2), (4, 'F', 0), (5, 'L', 4), (6, 'J', 4), (7, 'M', 2)
+	values (1, 'A', 2), (2, 'C', 4), (3, 'D', 2), (4, 'F', 0), (5, 'L', 4), (6, 'J', 4), (7, 'M', 2);
 
 insert into projects
-	values (1, 'B'), (2, 'L'), (3, 'M'), (4, 'K'), (5, 'X'), (6, 'F'), (7, 'Z')
+	values (1, 'B'), (2, 'L'), (3, 'M'), (4, 'K'), (5, 'X'), (6, 'F'), (7, 'Z');
 
 insert into employee_projects
-	values (1, 2), (2, 4), (3, 4), (4, 7), (5, 7), (6, 5), (7, 1)
+	values (1, 2), (2, 4), (3, 4), (4, 7), (5, 7), (6, 5), (7, 1);
 
 create table employee_levels as
 with employee_levels as (
@@ -36,8 +36,11 @@ with employee_levels as (
         join employee_levels p
         on c.manager_employee_id = p.employee_id
         )
-select * from employee_levels
+select * from employee_levels;
 
-select e.employee_id as most_senior_employee_id, p.project_id, min(e.level) as level_of_most_senior_employee from employee_levels as e, projects as p, employee_projects as ep 
+select e.employee_id 
+as most_senior_employee_id, p.project_id, min(e.level) 
+as level_of_most_senior_employee 
+from employee_levels as e, projects as p, employee_projects as ep 
 where e.employee_id = ep.employee_id and p.project_id = ep.project_id
-group by p.project_id
+group by p.project_id;
