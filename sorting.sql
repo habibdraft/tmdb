b2773074dbad063ab0eedb4dc091772169d96664
Nazia Habib <habib.nazia@gmail.com>
2021-12-20 11:29:48 -0600

    Delete sorting.sql

--- sorting.sql
+++
@@ -1,8 +0,0 @@
-create table sorting ( 
-	Input varchar(1)
-);
-
-insert into sorting
-	values ('A'), ('C'), ('D'), ('C'), ('D'), ('D'), ('B');
-	
-select Input as Output, unicode(Input)-64 as SortOrder from sorting order by SortOrder;
